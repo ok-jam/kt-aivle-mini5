13196ea2a00802e22531feeda7dd935a2c4aa925
201924611 <hu349401@gmail.com>
2025-07-02 03:03:20 +0000

    api설정

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? 'http://49.168.40.100:8080'  // 배포용
-    : 'http://localhost:8080',     // 로컬 개발용
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'http://49.168.40.100:8080'  // 배포 서버 주소
+    : 'http://localhost:8080',     // 로컬 서버 주소
 });
 
 export default api;
